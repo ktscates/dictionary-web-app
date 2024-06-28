275f91b6a70cf09654fe123ec735f3dbaefd699e
ktscates <cates2506@gmail.com>
2024-06-28 10:07:50 +0200

    project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-yellow-500">
+      <h1 className="font-sans_serif">Hello app component</h1>
+      <h1 className="font-serif">Hello app component</h1>
+      <h1 className="font-mono">Hello app component</h1>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        purple: "#A445ED",
+        red: "#FF5252",
+        black: "#050505",
+        gray: "#757575",
+        lightGray: "#E9E9E9",
+        veryLightG: "#F4F4F4",
+        veryDarkG: "#1F1F1F",
+        darkGray: "#2D2D2D",
+        grey: "#3A3A3A",
+      },
+      fontFamily: {
+        mono: ["Inconsolata", "monospace"],
+        sans_serif: ["Inter", "sans-serif"],
+        serif: ["Lora", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
